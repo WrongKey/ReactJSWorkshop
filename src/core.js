141383e5de8c59fb44822e3a59373f0d5801ae6e
Kai Hu <kwrongkey@gmail.com>
2017-06-11 21:51:02 +0800

    [Kai] initial workshop.

---
+++ src/core.js
@@ -0,0 +1,30 @@
+const NEIGHBOR_OFFSET = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+];
+
+const nextGeneration = (grid) =>
+    grid.map((row, rowIndex) =>
+        row.map((cell, colIndex) =>
+            getNextState(grid[rowIndex][colIndex], getAliveNeighborNumber([rowIndex, colIndex], grid))));
+
+const getAliveNeighborNumber = (position, grid) =>
+    NEIGHBOR_OFFSET.map(offset => toState(offset, position, grid)).reduce((result, state) => result + state, 0);
+
+const toState = (offset, position, grid) => {
+	const newRow = position[0] + offset[0];
+	const newColumn = position[1] + offset[1];
+	return withinGrid(newRow, newColumn, grid) ? grid[newRow][newColumn] : 0;
+};
+
+const getNextState = (currentState, aliveNeighborNumber) => [0, 0, currentState, 1, 0, 0, 0, 0, 0][aliveNeighborNumber];
+
+const withinGrid = (row, column, grid) => row >= 0 && row < grid.length && column >= 0 && column < grid[row].length;
+
+export default nextGeneration;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = <h3>Welcom to react workshop!</h3>;
+
+ReactDOM.render(app, document.getElementById('root'));
